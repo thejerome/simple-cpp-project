b7047f57cd649f825f70f772921b0bde3a198e87
Evgenii Efimchik <thejerome@mail.ru>
2018-09-05 19:07:24 +0300

    int tasks

--- src/main/cpp/minus.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Evgenii_Efimchik on 03.09.18.
-//
-
-#ifndef SIMPLE_CPP_PROJECT_MINUS_H
-#define SIMPLE_CPP_PROJECT_MINUS_H
-
-int minus_task();
-#endif //SIMPLE_CPP_PROJECT_MINUS_H
